032904c443e1ba99d127978876a36141e1deb520
bommisettiasha-create <bommisettiasha@gmail.com>
2026-08-26 19:32:59 +0530

    Refactor SpyNumber class formatting

--- SpyNumber.java
+++ SpyNumber.java
@@ -3,17 +3,13 @@ public class SpyNumber {
         int num = 123;
         int sum = 0;
         int product = 1;
-
-        while (num != 0) {
+    while (num != 0) {
             int digit = num % 10;
-
-            sum += digit;
+ sum += digit;
             product *= digit;
-
-            num /= 10;
+ num /= 10;
         }
-
-        if (sum == product)
+  if (sum == product)
             System.out.println("Spy Number");
         else
             System.out.println("Not Spy Number");
