40f884be2c40872fd0608e887efff03f6322acbf
bommisettiasha-create <bommisettiasha@gmail.com>
2026-08-26 19:31:12 +0530

    Add files via upload

---
+++ DuckNumber.java
@@ -0,0 +1,20 @@
+public class DuckNumber {
+    public static void main(String[] args) {
+        int num = 1023;
+        boolean duck = false;
+
+        while (num != 0) {
+            if (num % 10 == 0) {
+                duck = true;
+                break;
+            }
+
+            num /= 10;
+        }
+
+        if (duck)
+            System.out.println("Duck Number");
+        else
+            System.out.println("Not Duck Number");
+    }
+}
