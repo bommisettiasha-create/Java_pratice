141dcef7ac11eaf08fb536d0b678481038c64cf6
bommisettiasha-create <bommisettiasha@gmail.com>
2026-08-28 22:32:46 +0530

    Add files via upload

---
+++ HappyNumber.java
@@ -0,0 +1,27 @@
+public class HappyNumber {
+
+    static int sumOfSquares(int num) {
+        int sum = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int num = 19;
+
+        while (num != 1 && num != 4) {
+            num = sumOfSquares(num);
+        }
+
+        if (num == 1)
+            System.out.println("Happy Number");
+        else
+            System.out.println("Not Happy Number");
+    }
+}
