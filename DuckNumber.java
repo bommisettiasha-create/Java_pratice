d13ac811c66df560f35cc5504ca2c4773b6dd3d5
bommisettiasha-create <bommisettiasha@gmail.com>
2026-09-02 21:27:06 +0530

    Refactor DuckNumber.java for code style
    Reformatted code for better readability.

--- DuckNumber.java
+++ DuckNumber.java
@@ -2,19 +2,16 @@ public class DuckNumber {
     public static void main(String[] args) {
         int num = 1023;
         boolean duck = false;
-
-        while (num != 0) {
-            if (num % 10 == 0) {
-                duck = true;
-                break;
+ while (num != 0) {
+ if (num % 10 == 0) {
+duck = true;
+break;
             }
-
-            num /= 10;
+ num /= 10;
         }
-
-        if (duck)
-            System.out.println("Duck Number");
-        else
-            System.out.println("Not Duck Number");
+if (duck)
+ System.out.println("Duck Number");
+ else
+ System.out.println("Not Duck Number");
     }
 }
