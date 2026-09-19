b90dca5a2ad06acaf0a6a6c5d1729197e27007be
bommisettiasha-create <bommisettiasha@gmail.com>
2026-09-19 20:38:19 +0530

    Refactor RightRotate class formatting

--- RightRotate.java
+++ RightRotate.java
@@ -1,18 +1,13 @@
 public class RightRotate {
     public static void main(String[] args) {
-
-        int[] arr = {1, 2, 3, 4, 5};
-
-        int last = arr[arr.length - 1];
-
-        for (int i = arr.length - 1; i > 0; i--) {
-            arr[i] = arr[i - 1];
+int[] arr = {1, 2, 3, 4, 5};
+ int last = arr[arr.length - 1];
+ for (int i = arr.length - 1; i > 0; i--) {
+ arr[i] = arr[i - 1];
         }
-
-        arr[0] = last;
-
-        for (int num : arr) {
-            System.out.print(num + " ");
+ arr[0] = last;
+for (int num : arr) {
+ System.out.print(num + " ");
         }
     }
 }
