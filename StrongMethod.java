61a939ddf5761b463aa20c4d604ed0cdcfe51f59
bommisettiasha-create <bommisettiasha@gmail.com>
2026-09-20 21:42:57 +0530

    Refactor StrongMethod class formatting

--- StrongMethod.java
+++ StrongMethod.java
@@ -1,27 +1,21 @@
 public class StrongMethod {
-
-    static int factorial(int n) {
-        int fact = 1;
-
-        for (int i = 1; i <= n; i++) {
+ static int factorial(int n) {
+ int fact = 1;
+for (int i = 1; i <= n; i++) {
             fact *= i;
         }
-
-        return fact;
+ return fact;
     }
-
-    public static void main(String[] args) {
+ public static void main(String[] args) {
         int num = 145;
         int temp = num;
         int sum = 0;
-
-        while (temp != 0) {
+ while (temp != 0) {
             int digit = temp % 10;
             sum += factorial(digit);
             temp /= 10;
         }
-
-        if (sum == num)
+ if (sum == num)
             System.out.println("Strong Number");
         else
             System.out.println("Not Strong Number");
